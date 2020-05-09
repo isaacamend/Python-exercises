13911741693ca2a8afc7a90d349bb1b8fa2efd9c
Isaac Amend <isaacamend@Isaacs-Air.fios-router.home>
2020-05-09 14:59:41 -0400

    variables update

--- lab1.py
+++ lab1.py
@@ -7,3 +7,13 @@ print(x)
 # A variable name cannot start with a number
 # A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 # Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+# String variables can be declared either by using single or double quotes:
+x = "Isaac"
+y = 'Name'
+name = 'Helen'
+name = "Helen"
+flower = 'tulip'
+flower = "tulip"
+
+# variables can be called anything EXCEPT for keywords: https://www.w3schools.com/python/python_ref_keywords.asp
